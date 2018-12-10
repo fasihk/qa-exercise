243d4aaa9be029371470c27c30b850f7d3e30267
Fasih <fasihkhalili@gmail.com>
2018-12-10 18:36:55 +0000

    Add project files.

---
+++ ClassLibrary1/ValTechPageObjects/HomePage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+
+
+
+namespace ValTechPageObjectModel
+
+{
+    public class HomePage
+    {
+       private IWebDriver driverObj;
+
+       
+
+       
+
+    }
+}
